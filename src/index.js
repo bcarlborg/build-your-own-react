366ffb672b0d11ebc73514f56999ed1a65494fef
bcarlborg <bcarlborg@gmail.com>
2024-12-02 17:10:58 -0800

    step 4

--- src/index.js
+++ src/index.js
@@ -38,30 +38,111 @@ function createTextElement(text) {
 }
 
 /**
- * Render a react element to the DOM.
+ * Create a dom node for a fiber.
  */
-function render(element, container) {
-  // create a dom element for the root of our element
-  // tree
+function createDom(fiber) {
+  // Create a dom node for this fiber
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type);
 
   // add each property from our element to our dom node
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  // recursively render each child node into this
-  // dom node
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+  return dom;
 }
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+// =================================================================
+// Helpers for executing work loop on idle callback
+// =================================================================
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  //
+  // Create a dom node for this fiber and add it to the dom
+  //
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  //
+  // Create fibers for this fiber's children
+  // ensure each fiber references this fiber as its parent and the
+  // next fiber as its sibling
+  //
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+
+  //
+  // Return the next unit of work
+  // TODO(beau): Go over this algorithm and internalize how we traverse the fiber tree
+  //
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+
+// =================================================================
+// Exporting our library
+// =================================================================
 
 const Didact = { createElement, render };
 
